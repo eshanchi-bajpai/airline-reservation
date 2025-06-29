e1605e4306c714104afa7c3c25b4d80209acb838
eshanchi-bajpai <eshanchibajpai@gmail.com>
2025-06-30 00:10:48 +0200

    Add Booking repository interface

--- src/main/java/com/example/airline/repository/BookingRepository.java
+++ src/main/java/com/example/airline/repository/BookingRepository.java
@@ -2,8 +2,6 @@ package com.example.airline.repository;
 
 import com.example.airline.model.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface BookingRepository extends JpaRepository<Booking, Long> {
 }

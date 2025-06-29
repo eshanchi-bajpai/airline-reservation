6ddc059ae12e3da6bb7ac04fea287970ff480ff7
eshanchi-bajpai <eshanchibajpai@gmail.com>
2025-06-30 00:07:09 +0200

    Add User service class

--- src/main/java/com/example/airline/service/UserService.java
+++ src/main/java/com/example/airline/service/UserService.java
@@ -2,31 +2,27 @@ package com.example.airline.service;
 
 import com.example.airline.model.User;
 import com.example.airline.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class UserService {
+    private final UserRepository userRepository;
 
-    @Autowired
-    private UserRepository userRepository;
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
 
     public List<User> getAllUsers() {
         return userRepository.findAll();
     }
 
-    public Optional<User> getUserById(Long id) {
-        return userRepository.findById(id);
-    }
-
-    public User saveUser(User user) {
-        return userRepository.save(user);
-    }
-
-    public void deleteUser(Long id) {
-        userRepository.deleteById(id);
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
     }
 }
